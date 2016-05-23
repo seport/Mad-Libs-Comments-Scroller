00145c6e2fad0b214c01b977efd69b2b0bd449ee
sarah <sarahe.port@gmail.com>
2016-05-22 20:27:17 -0700

    fix mongoDB connection

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose')
-var url = process.env.MONGODB_URI || 'mongodb://localhost/commentsscroller'
+var url = 'mongodb://heroku_vmhqm4zh:p1lpgha3i5rhp9pgh0kto1pngk@ds011963.mlab.com:11963/heroku_vmhqm4zh'
 mongoose.connect(url)
 module.exports = mongoose
