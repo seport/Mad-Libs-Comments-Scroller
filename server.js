869f37f6f94143aa0efc1998c815e4702d867aa8
sarah <sarahe.port@gmail.com>
2016-05-22 20:33:53 -0700

    change index.html to ./index.html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express()
 app.use(bodyParser.json())
 
 app.get('/', function(req, res){
-	res.sendfile('index.html')
+	res.sendfile('./index.html')
 })
 
 app.get('/api/posts', function(req, res){
