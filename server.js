536f0a58507ba38d656f569326ead80ac379b963
sarah <sarahe.port@gmail.com>
2016-05-22 21:00:48 -0700

    CORS fix

--- server.js
+++ server.js
@@ -26,4 +26,15 @@ app.post('/api/posts', function(req, res, next){
 	})
 })
 
+var allowCrossDomain = function(req, res, next){
+	res.header('Access-Control-Allow-Origin', "*")
+	res.header('Access-Control-Allow-Methods', "GET")
+	res.header('Access-Control-Allow-Headers', 'Content-Type')
+	next()
+}
+
+app.configure(function(){
+	app.use(allowCrossDomain);
+})
+
 app.listen(process.env.PORT || 8080)
