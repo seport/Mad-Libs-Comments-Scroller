e3fa7faf065bff3b1657a36f3c04fbe73b73d6ae
sarah <sarahe.port@gmail.com>
2016-05-24 22:27:06 -0700

    CORS fix

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var bodyParser = require('body-parser')
 var app = express()
 app.use(bodyParser.json())
 
+app.use(function(req,res,next){
+	res.header('Access-Control-Allow-Origin',"*");
+	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers','Content-Type');
+})
+
 app.get('/', function(req, res){
 	res.sendfile('index.html')
 })
@@ -25,24 +31,5 @@ app.post('/api/posts', function(req, res, next){
 		res.json(201, post)
 	})
 })
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 app.listen(process.env.PORT || 8080)
