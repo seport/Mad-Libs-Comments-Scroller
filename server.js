f57329ecffe1e5f5dd375f2c1d4d8ed60920a103
Sarah Port <sarah@carbonfive.com>
2018-10-02 12:45:22 -0700

    install socket.io on server and attendee client

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var socket = require('socket.io')
 
 var app = express()
 app.use(bodyParser.json())
@@ -15,32 +16,41 @@ app.get('/', function(req, res){
 	res.sendfile('index.html')
 })
 
-app.get('/api/posts', function(req, res){
-	Post.find(function(err, posts){
-		if(err){ return next(err) }
-		res.json(posts)
-	})
-})
+// app.get('/api/posts', function(req, res){
+// 	Post.find(function(err, posts){
+// 		if(err){ return next(err) }
+// 		res.json(posts)
+// 	})
+// })
 
-var Post = require('./models/post')
-app.post('/api/posts', function(req, res, next){
+// var Post = require('./models/post')
+// app.post('/api/posts', function(req, res, next){
 	
-	var post = new Post({
-		word: req.body.word
-	})
-	post.save(function(err, post){
-		if(err){ return next(err) }
-		res.json(201, post)
-	})
+// 	var post = new Post({
+// 		word: req.body.word
+// 	})
+// 	post.save(function(err, post){
+// 		if(err){ return next(err) }
+// 		res.json(201, post)
+// 	})
+// })
+
+// app.delete('/api/posts/:id', function(req, res, next){
+// 	Post.findOneAndRemove({'_id': req.params.id}, function(err, result){
+// 		res.json({
+// 			message: "successfully deleted the post",
+// 			post: result
+// 		})
+// 	})
+// })
+
+var server = app.listen(process.env.PORT || 8080, function(){
+	console.log(`listening on port ${process.env.PORT || 8080}`)
 })
 
-app.delete('/api/posts/:id', function(req, res, next){
-	Post.findOneAndRemove({'_id': req.params.id}, function(err, result){
-		res.json({
-			message: "successfully deleted the post",
-			post: result
-		})
-	})
-})
+// socket setup
+var io = socket(server)
 
-app.listen(process.env.PORT || 8080)
+io.on('connection', function(socket){
+	console.log('made socket connection', socket.id)
+})
